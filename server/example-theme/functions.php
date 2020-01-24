0c3c56be54b0be6b02bc50e19c427c742ec622f9
Gert Glükmann <gglukmann@gmail.com>
2020-01-24 17:15:50 +0200

    Remove unused code

--- server/example-theme/functions.php
+++ server/example-theme/functions.php
@@ -11,25 +11,3 @@ add_action('rest_api_init', function () {
     });
   }
 });
-
-// if (!function_exists('acf_nullify_empty')) {
-//   /**
-//    * Return `null` if an empty value is returned from ACF.
-//    * Because otherwise GraphQL would get two different types for same field. object and boolean
-//    * Now it will return `null` if translation is not added and not `false` as boolean value.
-//    *
-//    * @param mixed $value
-//    * @param mixed $post_id
-//    * @param array $field
-//    *
-//    * @return mixed
-//    */
-//   function acf_nullify_empty($value, $post_id, $field)
-//   {
-//     if (empty($value)) {
-//       return null;
-//     }
-//     return $value;
-//   }
-// }
-// add_filter('acf/format_value', 'acf_nullify_empty', 100, 3);
